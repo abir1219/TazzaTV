e0882619186ef2ace3f21324fe2d3796fc8d3426
abirchanda <jbmmatrix.150@gmail.com>
2022-04-29 17:24:25 +0530

    commit

---
+++ app/src/main/java/com/textifly/tazzatv/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.textifly.tazzatv.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.textifly.tazzatv.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/textifly/tazzatv/ui/Home/HomeFragment.java
+++ app/src/main/java/com/textifly/tazzatv/ui/Home/HomeFragment.java
@@ -8,11 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.textifly.tazzatv.MainActivity;
 import com.textifly.tazzatv.R;
 import com.textifly.tazzatv.databinding.FragmentHomeBinding;
+import com.textifly.tazzatv.ui.Home.Adapter.ViewPagerAdapter;
+import com.textifly.tazzatv.ui.Home.AllNews.AllNewsFragment;
+import com.textifly.tazzatv.ui.Home.IPL.IplFragment;
+import com.textifly.tazzatv.ui.Home.NationalNews.NationalNewsFragment;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
     FragmentHomeBinding binding;
 
     @Override
@@ -20,8 +25,40 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater,container,false);
-
+        btnClick();
+        setViewPager();
 
         return binding.getRoot();
+    }
+
+    private void btnClick() {
+        binding.llMenu.setOnClickListener(this);
+    }
+
+    private void setViewPager() {
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+
+        // add your fragments
+        adapter.addFrag(new AllNewsFragment(), "Home");
+        adapter.addFrag(new IplFragment(), "IPL 2022");
+        adapter.addFrag(new NationalNewsFragment(), "India");
+        adapter.addFrag(new NationalNewsFragment(), "Photo");
+        adapter.addFrag(new NationalNewsFragment(), "Video");
+        adapter.addFrag(new NationalNewsFragment(), "Entertainment");
+
+        binding.viewPager.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.llMenu:
+                ((MainActivity)getActivity()).openDrawer();
+                break;
+        }
     }
 }
